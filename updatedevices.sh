64785adbb6601ab547db75b18acb94e61a2a170e
martin <blades2000@gmail.com>
2014-11-08 20:22:05 +0000

    updated with comments

--- updatedevices.sh
+++ updatedevices.sh
@@ -2,4 +2,7 @@
 
 echo Transferring $1
 # this will transfer the file and unzip it host machine
-echo $1 | parallel --sshloginfile ipaddresses  --transfer "rm $1; gunzip {}"
+#echo $1 | parallel --sshloginfile ipaddresses  --transfer gunzip {}
+
+#another way of doing it with out the echo command
+parallel --sshloginfile ipaddresses  --transfer gunzip ::: $1 
